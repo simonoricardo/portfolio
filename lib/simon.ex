17d54e9b1fb0b60d0ed43a678f8e475548c06f84
Simon Ricard <ricardsimon@rumandcode.io>
2024-05-21 12:00:46 -0400

    main - Init

---
+++ lib/simon.ex
@@ -0,0 +1,18 @@
+defmodule Simon do
+  @moduledoc """
+  Documentation for `Simon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Simon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
