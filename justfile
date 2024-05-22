17c8786502bcccfb4a72f4cab2223e8516593da1
Simon Ricard <ricardsimon@rumandcode.io>
2024-05-22 12:36:58 -0400

    main - WIP

---
+++ justfile
@@ -0,0 +1,11 @@
+set positional-arguments
+
+[private]
+default:
+  just --list
+
+@build:
+  mix site.build
+
+@dev:
+  cd output && npx hostr -p 8000
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Simon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -18,11 +19,19 @@ defmodule Simon.MixProject do
     ]
   end
 
+  defp aliases() do
+    [
+      "site.build": ["build", "tailwind default --minify", "esbuild default"]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:nimble_publisher, "~> 1.1.0"},
+      {:phoenix_live_view, "~> 1.0.0-rc.0"},
+      {:esbuild, "~> 0.8.1"},
+      {:tailwind, "~> 0.2.2"}
     ]
   end
 end
