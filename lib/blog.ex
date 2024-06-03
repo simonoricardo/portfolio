82a73b49d40889184cb6422f48574182705466c2
Simon Ricard <ricardsimon@rumandcode.io>
2024-06-02 20:26:36 -0400

    main - Posts with automatic year parsing

--- lib/blog.ex
+++ lib/blog.ex
@@ -8,4 +8,33 @@ defmodule Website.Blog do
   @posts Enum.sort_by(@posts, & &1.date, {:desc, Date})
 
   def all_posts, do: @posts
+
+  def by_year(posts) do
+    Enum.reduce(get_years_with_posts(posts), %{}, fn year, acc ->
+      posts =
+        Enum.map(posts, fn post ->
+          if post_year(post) == year do
+            post
+          end
+        end)
+        |> Enum.reject(&is_nil(&1))
+
+      Map.put(acc, year, posts)
+    end)
+  end
+
+  def get_years_with_posts(posts) do
+    Enum.reduce(posts, [], fn post, acc ->
+      post_year = post_year(post)
+
+      if not Enum.member?(acc, post_year) do
+        [post_year | acc]
+      else
+        acc
+      end
+    end)
+    |> Enum.sort(:desc)
+  end
+
+  defp post_year(post), do: Date.to_string(post.date) |> String.slice(0, 4)
 end
---
+++ posts/2024/05-26-test.md
@@ -0,0 +1,11 @@
+%{
+  title: "It's alive!",
+  author: "Simon Ricard",
+  tags: ~w(post blog portfolio elixir static markdown),
+  description: "My blog is alive! How was it built?"
+}
+---
+
+## Gotcha! Not a real post.
+
+This is very funny.
